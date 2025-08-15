6fdf6222542f632cd357dfb601095cca3e52b4ec
abi779 <abigailplatamarquez47@gmail.com>
2025-08-14 23:33:07 -0300

    Crear tarea7.py
    holaaaaa

---
+++ tarea7.py
@@ -0,0 +1,14 @@
+class Clase:
+    def __init__(self, atributo):
+        #init va con dos guiones bajos al inicio y al final
+        self.atributo = atributo
+
+    def metodo(self):
+        return "El atributo es: " + self.atributo
+    
+    def imprimir(self):
+        print("Imprimiendo: " + self.atributo )
+
+instancia = Clase("valor")
+print(instancia.metodo() )
+instancia.imprimir()
