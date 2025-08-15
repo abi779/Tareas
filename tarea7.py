8e740117bf7bd06f0de3c81de4e9537ccc169503
abi779 <abigailplatamarquez47@gmail.com>
2025-08-14 23:46:31 -0300

    Actualizar tarea7.py

--- tarea7.py
+++ tarea7.py
@@ -12,3 +12,14 @@ class Clase:
 instancia = Clase("valor")
 print(instancia.metodo() )
 instancia.imprimir()
+#ejercicio: crear una clase que sume dos numeros y los muestre por pantalla
+class Suma:
+    def __init__(self, numero1, numero2):
+        self.numero1 = numero1
+        self.numero2 = numero2
+
+    def calcular_suma(self):
+        return self.numero1 + self.numero2
+
+    def mostrar_resultado(self):
+        print("La suma es: " + str(self.calcular_s
