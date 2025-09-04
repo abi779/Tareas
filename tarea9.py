1b798903b871ebb447b801ab47cd8bb3529ba04a
abi779 <abigailplatamarquez47@gmail.com>
2025-09-04 13:24:28 -0300

    Actualizar tarea9.py

--- tarea9.py
+++ tarea9.py
@@ -1,29 +1,40 @@
-import csv
-import json
-class Persona:
-    def __init__(self,name,age,isStudent):
-        self.name=name
-        self.age=age
-        self.isStudent=isStudent
-    def __str__(self):
-        return "nombre: "+self.name + ",edad: " + self.age + ",es estudiante: " + self.isStudent
-personas = []
+import math
+raiz_cuadrada = math.sqrt(144)
+entero = int(raiz_cuadrada)
+#INTEGER
+raiz_cubica = math.cbrt(entero)
+entero = int(raiz_cubica)
+#podemos hacer raices elevando un 
+# número a un exponente de fracción
+igual = raiz_cuadrada == 144 ** (1/2)
+#usar math.pow es lo mismo que usar **
+numero_al_cuadrado = math.pow(144,2)
+print(entero)
+raiz_cuadratica = 16 ** (1/4)
+print(raiz_cuadratica)
+#print(igual)
+numero_pi=math.pi
+print(numero_pi)
+pi_al_cuadrado = numero_pi ** 2
+print(pi_al_cuadrado)
+numero_de_euler = math.e
+print(numero_de_euler)
+logaritmo_base_e = math.log(144)
+print(logaritmo_base_e)
+logaritmo_base_2 = math.log2(144)
+print(logaritmo_base_2)
+infinito = math.inf
+print(infinito/2)
 
-with open('data.csv','r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        persona = Persona(row[0],row[1],row[2])
-        personas.append(persona)
-personas.pop(0)
-for persona in personas:
-    print(persona.__str__())
-'''
-{
-  "name": "John Doe",
-  "age": 30,
-  "isStudent": false
-}
+def raiz(radicando,indice):
+    return radicando ** (1/indice)
 
-name,age,isStudent
-John Doe,30,false
-'''
+raiz_novena = raiz(512,9)
+logaritmo = math.log2(512)
+print(logaritmo)
+numero_pi = -numero_pi
+truncar = math.trunc(numero_pi)
+entero = int(numero_pi)
+redondear = math.floor(numero_pi)
+absoluto = math.fabs(redondear)
+print(entero,truncar,redondear,absoluto)
